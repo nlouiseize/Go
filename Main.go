45f1f1f7c30dd740e8628c8b11a80471626947d6
nlouiseize <107628590+nlouiseize@users.noreply.github.com>
2023-02-28 13:39:57 -0500

    Add files via upload

---
+++ Main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// This is only for this example.
+// Please handle errors properly.
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+var (
+	hostName        = "10.128.2.17"
+	ClusterPort int = 3000
+	namespace       = "test"
+	setName         = "peeps"
+	myKey           = "1"
+	GoPort          = flag.Int("p", 8080, "server port")
+	name            = "nelzir"
+	age             = "30"
+)
+
+func main() {
+
+	tmpl, err := template.ParseFiles("./Templates/Aerospike-Inputs.html")
+	if err != nil {
+		log.Fatal("Parse: ", err)
+		return
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+		fmt.Println("Server works!")
+
+		hostName = r.FormValue("HostIP")
+		namespace = r.FormValue("namespace")
+		setName = r.FormValue("set")
+		myKey = r.FormValue("PK")
+		name = r.FormValue("name")
+		age = r.FormValue("age")
+		fmt.Println(name, age)
+		
+	})
+	http.ListenAndServe(":8000", nil)
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module docker-aerospike
+
+go 1.19
+
+require github.com/aerospike/aerospike-client-go v4.5.2+incompatible
+
+require (
+	github.com/onsi/ginkgo v1.16.4 // indirect
+	github.com/onsi/gomega v1.15.0 // indirect
+	github.com/reloadly/reloadly-sdk-golang v1.0.1 // indirect
+	github.com/yuin/gopher-lua v0.0.0-20200816102855-ee81675732da // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
