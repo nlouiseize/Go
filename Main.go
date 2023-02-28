82b76b17f62af06c62f95a291afe15272525f7ff
nlouiseize <107628590+nlouiseize@users.noreply.github.com>
2023-02-28 14:56:12 -0500

    Add files via upload

--- Main.go
+++ Main.go
@@ -29,14 +29,17 @@ var (
 
 func main() {
 
-	tmpl, err := template.ParseFiles("Aerospike-Inputs.html")
+	// An HTML template
+	const tmpl = "\n<div>\n    {{if .Success}}\n    <div>\n        <p>Your top-up is successful!</p>\n    </div>\n    {{else}}\n    <div>\n        <h1>Aerospike Inputs</h1>\n    </div>\n    <div></div>\n    <form action=\"/aerospike-inputs.html\" method=\"POST\">\n        <input\n                type=\"text\" placeholder=\"your host IP\" name=\"HostIP\"\n        />\n        <input\n                type=\"text\" placeholder=\"your namespace\" name=\"namespace\"\n        />\n        <input\n                type=\"text\" placeholder=\"your set\" name=\"set\"\n        />\n        <input\n                type=\"text\" placeholder=\"your PK\" name=\"PK\"\n        />\n        <input\n                type=\"text\" placeholder=\"your name\" name=\"name\"\n        />\n        <input\n                type=\"text\" placeholder=\"your age\" name=\"age\"\n        />\n        <input  type=\"submit\" value=\"submit\" />\n    </form>\n    {{end}}\n</div>"
+
+	t, err := template.New("Aerospike-Inputs").Parse(tmpl)
 	if err != nil {
 		log.Fatal("Parse: ", err)
 		return
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
+			t.Execute(w, nil)
 			return
 		}
 		fmt.Println("Server works!")
@@ -48,7 +51,7 @@ func main() {
 		name = r.FormValue("name")
 		age = r.FormValue("age")
 		fmt.Println(name, age)
-		
+
 	})
 	http.ListenAndServe(":8000", nil)
 }
